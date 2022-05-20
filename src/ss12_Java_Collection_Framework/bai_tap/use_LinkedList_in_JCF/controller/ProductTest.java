92f362d095323d925525c373d5ce47ef97f17c8f
unknown <you@example.com>
2022-05-20 19:37:58 +0700

    ualdknp;m

--- src/ss12_Java_Collection_Framework/bai_tap/use_LinkedList_in_JCF/controller/ProductTest.java
+++ src/ss12_Java_Collection_Framework/bai_tap/use_LinkedList_in_JCF/controller/ProductTest.java
@@ -7,9 +7,9 @@ import java.util.Scanner;
 
 public class ProductTest {
     private static Scanner scanner = new Scanner(System.in);
+    static IProductService productService = new ProductManager();
 
     public void displayMain() {
-        IProductService productService = new ProductManager();
         do {
             System.out.println("____Product Management____");
             System.out.println("1. Display List Product.");
@@ -17,28 +17,13 @@ public class ProductTest {
             System.out.println("3. Search Product.");
             System.out.println("4. Delete Product.");
             System.out.println("5. Update Product.");
-            System.out.println("6. Exit.");
+            System.out.println("6. Display Product Sorted By Price.");
+            System.out.println("7. Exit.");
             System.out.println("Choose Options: ");
             int choose = Integer.parseInt(scanner.nextLine());
             switch (choose) {
                 case 1:
-                    System.out.println("Display List Product: ");
-                    System.out.println("1. Hiển Thị Danh sách Theo Id: ");
-                    System.out.println("2. Hiển Thị Danh sách Tăng dần theo Giá: ");
-                    System.out.println("3. Hiển Thị Danh sách Giảm dần theo Giá: ");
-                    int inputDisplay = Integer.parseInt(scanner.nextLine());
-                    if (inputDisplay == 1) {
-                        System.out.println("1. Danh sách Theo Id: ");
-                        productService.displayListProduct();
-                    } else if (inputDisplay == 2) {
-                        System.out.println("2. Danh sách Tăng theo Giá: ");
-                        productService.displayListProductByInCrease();
-                    } else if (inputDisplay == 3) {
-                        System.out.println("3. Danh sách Giảm theo Giá: ");
-                        productService.displayListProductByDecrease();
-                    } else {
-                        System.out.println("Nhập lại từ 1 -> 3 !!");
-                    }
+                    productService.displayListProduct();
                     break;
                 case 2:
                     productService.addNewProduct();
@@ -53,9 +38,27 @@ public class ProductTest {
                     productService.updateProductById();
                     break;
                 case 6:
+                    displaySortListProduct();
+                    break;
+                case 7:
                     System.exit(0);
             }
         }
         while (true);
+    }
+
+    public void displaySortListProduct() {
+        System.out.println("1. Hiển Thị Danh sách Tăng dần theo Giá: ");
+        System.out.println("2. Hiển Thị Danh sách Giảm dần theo Giá: ");
+        int inputDisplay = Integer.parseInt(scanner.nextLine());
+        if (inputDisplay == 1) {
+            System.out.println("1. Danh sách Tăng dần theo Giá: ");
+            productService.displayListProductByInCrease();
+        } else if (inputDisplay == 2) {
+            System.out.println("2. Danh sách Giảm dần theo Giá: ");
+            productService.displayListProductByDecrease();
+        } else {
+            System.out.println("Nhập lại từ 1 -> 3 !!");
+        }
     }
 }
