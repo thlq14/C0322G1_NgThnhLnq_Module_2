6d3dce83825881f3e31450e74f4fea507a71938c
unknown <you@example.com>
2022-05-30 18:57:35 +0700

    kakjbkse

--- src/ss19_String_Regex/bai_tap/validate_class_name/ClassName.java
+++ src/ss19_String_Regex/bai_tap/validate_class_name/ClassName.java
@@ -1,17 +1,20 @@
 package ss19_String_Regex.bai_tap.validate_class_name;
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class ClassName {
     public static void main(String[] args) {
         String regex = "[C|A|P][0-9]{4}[G|H|I|K|L|M]";
-        String nameClass = "C0318G";
+        System.out.println("Nhập Tên Lớp Cần Kiểm Tra: ");
+        Scanner scanner = new Scanner(System.in);
+        String nameClass = scanner.nextLine();
 
         boolean checkName = Pattern.matches(regex, nameClass);
         if (checkName) {
-            System.out.println(nameClass + " hợp lệ.");
+            System.out.println("Lớp " + nameClass + " hợp lệ.");
         } else {
-            System.out.println(nameClass + " KHÔNG hợp lệ.");
+            System.out.println("Lớp " + nameClass + " KHÔNG hợp lệ.");
         }
     }
 }
--- src/ss19_String_Regex/bai_tap/validate_phone_number/PhoneNumber.java
+++ src/ss19_String_Regex/bai_tap/validate_phone_number/PhoneNumber.java
@@ -1,16 +1,20 @@
 package ss19_String_Regex.bai_tap.validate_phone_number;
+
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class PhoneNumber {
     public static void main(String[] args) {
         String regexPhoneNumber = "[0-9]{2}-[0][0-9]{9}";
-        String phone = "84-0978489648";
+        System.out.println("Nhập SĐT Cần Kiểm Tra: ");
+        Scanner scanner = new Scanner(System.in);
+        String phone = scanner.nextLine();
 
         boolean checkPhone = Pattern.matches(regexPhoneNumber, phone);
         if (checkPhone) {
-            System.out.println(phone + " là sđt hợp lệ.");
+            System.out.println("SĐT: " + phone + " hợp lệ.");
         } else {
-            System.out.println(phone + " KHÔNG phải là sđt hợp lệ.");
+            System.out.println("SĐT: " + phone + " KHÔNG hợp lệ.");
         }
     }
 }
